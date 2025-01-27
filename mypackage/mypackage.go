c44a4196270f1a8cc261426ef40cf6f6a2b496e2
Sarvesh Raj <saraj@rippling.com>
2025-01-28 01:27:52 +0530

    Update ConvIntToStr function to support integers from 1 to 20

--- mypackage/mypackage.go
+++ mypackage/mypackage.go
@@ -2,7 +2,7 @@ package mypackage
 
 import "errors"
 
-// ConvIntToStr returns the word for the given integer (1-9) or an error if out of range
+// ConvIntToStr returns the word for the given integer (1-20) or an error if out of range
 func ConvIntToStr(n int) (string, error) {
 	switch n {
 	case 1:
@@ -23,6 +23,28 @@ func ConvIntToStr(n int) (string, error) {
 		return "Eight", nil
 	case 9:
 		return "Nine", nil
+	case 10:
+		return "Ten", nil
+	case 11:
+		return "Eleven", nil
+	case 12:
+		return "Twelve", nil
+	case 13:
+		return "Thirteen", nil
+	case 14:
+		return "Fourteen", nil
+	case 15:
+		return "Fifteen", nil
+	case 16:
+		return "Sixteen", nil
+	case 17:
+		return "Seventeen", nil
+	case 18:
+		return "Eighteen", nil
+	case 19:
+		return "Nineteen", nil
+	case 20:
+		return "Twenty", nil
 	default:
 		return "", errors.New("number out of range")
 	}
